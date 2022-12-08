f407d56d15356542779e2cedbd5ceeaf2df2bd1a
katya20220910 <katyaantonova009@gmail.com>
2022-12-08 22:28:34 +0200

    Up

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// Описан в документации
+
 import SimpleLightbox from 'simplelightbox';
-// Дополнительный импорт стилей
+
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImages } from '../src/api';
 
